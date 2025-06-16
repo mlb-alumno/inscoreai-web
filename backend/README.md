966a608a23062b2f1aa598f04c2fbdb45b19fee9
mlb-alumno <m.lallanab.alumno@gmail.com>
2025-06-16 11:57:39 +0200

    inital commit

--- backend/README.md
+++
@@ -1,1 +0,0 @@
-Web For CoMuLab
--- backend/anticipation/__init__.py
+++
@@ -1,9 +0,0 @@
-""" Infrastructure for constructing anticipatory infilling models.
-
-This model provides infrastructure to preprocess Midi music datasets
-for training anticipatory music infilling models. For more context, see:
-
-    Anticipatory Music Transformer
-    John Thickstun, David Hall, Chris Donahue, Percy Liang
-    Preprint Report, 2023
-"""
